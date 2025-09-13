a3dd2994ceff7955b48c4f6fac4fc063fc3f7a07
Pratik1joshi <jcprakash555@gmail.com>
2025-09-13 22:18:47 +0545

    Callback solved

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -86,7 +86,7 @@ export default function LoginPage() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `https://quick-work-mu.vercel.app/auth/callback?next=/dashboard`,
+          redirectTo: `${window.location.origin}/auth/callback?next=/dashboard`,
         },
       })
 
